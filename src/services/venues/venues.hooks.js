400785ed88be0c0e418f6788477e90a43abd85e8
Ilian Pavlov <zorqie@yahoo.com>
2018-02-09 15:24:09 -0800

    Moved auth from all to modify methods

--- src/services/venues/venues.hooks.js
+++ src/services/venues/venues.hooks.js
@@ -1,20 +1,35 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const hooks = require('feathers-hooks-common');
+
+const schema = {
+  service: 'venues',
+  include: [{
+    service: 'gigs',
+    nameAs: 'gigs',
+    asArray: true,
+    query: { $sort: {start: 1} },
+    parentField: '_id',
+    childField: 'venue_id',
+  }]  
+}
+
+
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [  ],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [authenticate('jwt')],
+    update: [authenticate('jwt')],
+    patch: [authenticate('jwt')],
+    remove: [authenticate('jwt')]
   },
 
   after: {
     all: [],
     find: [],
-    get: [],
+    get: [hooks.populate({schema})],
     create: [],
     update: [],
     patch: [],
